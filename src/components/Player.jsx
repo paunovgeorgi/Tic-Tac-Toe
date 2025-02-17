da1722f1938132557107d2c3aaed338447faf39b
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2025-02-17 19:08:52 +0200

    edit name logic implemented

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,14 +1,30 @@
+import { useState } from "react";
 
-const Player = ({name, symbol}) => {
+const Player = ({ name, symbol }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(name);
+
+  function handleEditClick() {
+    setIsEditing((prev) => (!prev));
+  }
+
+  function handleChange(e){
+    setPlayerName(e.target.value)
+  }
+
   return (
     <li>
-    <span className='player'>
-      <span className="player-name">{name}</span>
-      <span className="player-symbol">{symbol}</span>
-    </span>
-    <button>Edit</button>
-  </li>
-  )
-}
+      <span className="player">
+        {isEditing ? (
+          <input value={playerName} onChange={handleChange}/>
+        ) : (
+          <span className="player-name">{playerName}</span>
+        )}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{!isEditing ? "Edit" : "Save"}</button>
+    </li>
+  );
+};
 
-export default Player
+export default Player;
